eee2d8f4f02c4bebdee09097a053f7b7cec7a16c
middle-ages <ran@haag59.com>
2026-06-17 00:01:23 -0700

    a

--- src/Demo/StylePanel/index.tsx
+++ src/Demo/StylePanel/index.tsx
@@ -16,7 +16,7 @@ export const StylePanel = () => {
 
   return (
     <Details label='Appearance'>
-      <div className='grid grid-cols-[10ch_1fr] gap-x-2 gap-y-0.5 text-fg-control'>
+      <div className='grid grid-cols-[10ch_1fr] gap-x-2 gap-y-0.5'>
         <Row label='Label format'>
           <Select<NumericFormat>
             value={{...formats[format], id: format}}
