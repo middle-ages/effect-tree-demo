651dd7eaf72dc57fac57dc2677076cfdd4312d12
middle-ages <ran@haag59.com>
2026-06-21 12:12:07 -0700

    a

--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -2,6 +2,5 @@ import {withClassName, type WithClassName} from '#compinators'
 import type {Types} from 'effect'
 import type {FC} from 'react'
 
-export const Pill: FC<Types.Simplify<WithClassName<'div'>>> = withClassName.div(
-  'pill-panel border inset-xy bg-black/20',
-)
+export const Pill: FC<Types.Simplify<WithClassName<'div'>>> =
+  withClassName.div('pill-panel')
